f61ecbda0f5555403389fad712df6060cb0d93c1
Brendan Forster <brendan@github.com>
2017-04-07 18:44:47 +1000

    closer

--- script/build.sh
+++ script/build.sh
@@ -1,1 +1,11 @@
-echo "Hello $WORLD_VARIABLE!"
+
+if [ "$PLATFORM" == "ubuntu" ]; then
+  echo "Building Git for Ubuntu"
+elif [ "$PLATFORM" == "macOS" ]; then
+  echo "Building Git for macOS"
+elif [ "$PLATFORM" == "win32" ]; then
+  echo "Building Git for Windows"
+else
+  echo "Unable to build Git for platform $PLATFORM"
+  exit 1
+fi
