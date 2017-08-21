8d56bff7edf70851449baa65e3f3fe2cdbe51bfa
Brendan Forster <brendan@github.com>
2017-08-21 10:21:53 +1000

    poke at the output

--- script/eslint.ts
+++ script/eslint.ts
@@ -16,11 +16,40 @@ const opts = {
 }
 
 if (process.env.CI) {
-  ChildProcess.spawn(
+  const cp = ChildProcess.spawn(
     Path.resolve(__dirname, '..', 'node_modules', '.bin', 'eslint'),
     ESLINT_ARGS,
     opts
   )
+
+  const stdout = new Array<Buffer>()
+  cp.stdout.on('data', chunk => {
+    stdout.push(chunk as Buffer)
+  })
+
+  const stderr = new Array<Buffer>()
+  cp.stderr.on('data', chunk => {
+    stderr.push(chunk as Buffer)
+  })
+
+  cp.stdout.once('close', () => {
+    const output = Buffer.concat(stdout)
+    console.log(`output: '${output}'`)
+  })
+
+  cp.stderr.once('close', () => {
+    const error = Buffer.concat(stderr)
+    console.log(`error: '${error}'`)
+  })
+
+  cp.on('error', err => {
+    console.log(`error raised: ${err}`)
+  })
+
+  cp.on('exit', (code, signal) => {
+    console.log(`exit with code: ${code}`)
+  })
+
 } else {
   console.log('> Spinning up eslint_d\n')
   require('eslint_d/lib/client').lint(ESLINT_ARGS)
